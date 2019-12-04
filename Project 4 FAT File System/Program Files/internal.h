368b8d6c78d2d08ab2a0be017fd655801509533d
BioticComponent <joeyu@Joes-MacBook-Pro.local>
2019-12-03 19:34:15 -0500

    bring shell to project 4

---
+++ Project 4 FAT File System/Program Files/internal.h
@@ -0,0 +1,11 @@
+//Joe Uzdzinski
+//internal.h
+
+#ifndef INTERNAL_H
+#define INTERNAL_H
+
+void internalCommandHandler(INPUT* splitInput, ENV* env, CMDINFO* cmdinfo);
+int checkInternal (INPUT* splitInput);
+
+
+#endif
